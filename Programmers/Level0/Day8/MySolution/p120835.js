68f749c4ef6661ea707de434f50c10205b16069f
yeeunlee <euneee2000@naver.com>
2023-06-23 00:40:56 +0900

    Summerizing Day8 programming

--- Programmers/Level0/Day8/MySolution/p120835.js
+++ Programmers/Level0/Day8/MySolution/p120835.js
@@ -34,6 +34,7 @@ function solution(emergency) {
 const emergency1 = [3, 76, 24];
 console.log(solution(emergency1));
 
+// PASS
 // ERROR : 어느 부분에서 정렬이 안되는지 모르겠다! ㅜㅜ
 // 1. 배열 emergency를 오름차순으로 정렬하기
 // 1-2. 가장 작은 숫자를 찾아 순위 매기기 (인덱스 번호를 저장해두기)
