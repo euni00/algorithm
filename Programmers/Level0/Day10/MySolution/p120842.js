1c6c2dd7a029702ef98a08d0a257e9271e359553
yeeunlee <euneee2000@naver.com>
2023-06-25 23:11:44 +0900

    Studying Day10 array programming

--- Programmers/Level0/Day10/MySolution/p120842.js
+++ Programmers/Level0/Day10/MySolution/p120842.js
@@ -1,20 +1,26 @@
 // 2차원으로 만들기
 // 스스로 해결하기
 function solution(num_list, n) {
-  var newArray = [[]];
-  let numArray = [];
+  var newArray = [];
 
-  for (let i = 0; i < num_list; i++) {
-    // 1. 배열 numArray에 num_list 배열의 n개 만큼 넣기
-    for (let j = 0; j < n; j++) {
+  /*
+  while (num_list.length !== 0) {
+    let numArray = [];
+
+    for (let i = 0; i < n; i++) {
       numArray.push(num_list[i]);
     }
 
-    num_list.splice(1, n);
+    num_list.splice(0, n);
 
-    // 2. newArray에 numArray 넣어주기
+    // console.log("numArray : ", numArray);
+
     newArray.push(numArray);
+
+    // console.log("newArray : ", newArray);
   }
+  */
+
   return newArray;
 }
 
@@ -23,8 +29,12 @@ const num_list1 = [1, 2, 3, 4, 5, 6, 7, 8];
 const n1 = 2;
 console.log(solution(num_list1, n1));
 
-// ERROR : [ [] ]
-// 1. numArray에 num_list 배열의 n개 만큼 넣기
-// 2. numArray에 넣은 숫자는 삭제해주기
-// 2. newArray에 numArray 넣어주기
-// 3. num_list 배열 길이만큼 반복하기
+// test2
+const num_list2 = [100, 95, 2, 4, 5, 6, 18, 33, 948];
+const n2 = 3;
+console.log(solution(num_list2, n2));
+
+// PASS
+// 1. 배열 num_list에서 배열요소 n개를 새로운 배열에 넣기
+// 2. 새로운 배열에 넣은 숫자는 제거하기
+// 2. 배열을 다시 newArray에 넣어주기
--- Programmers/Level0/Day10/MySolution/p120843.js
+++ Programmers/Level0/Day10/MySolution/p120843.js
@@ -1,15 +1,16 @@
 // 공 던지기
 // 스스로 해결하기
 function solution(numbers, k) {
-  // var answerNum = 0;
+  let personNum = 1;
 
-  let personNum = 3 * k;
-
-  while (personNum < numbers.length) {
-    personNum = personNum - numbers.length;
+  for (let i = 0; i < k - 1; i++) {
+    personNum += 2;
+    if (personNum > numbers.length) {
+      personNum -= numbers.length;
+    }
   }
 
-  return personNum + 1;
+  return personNum;
 }
 
 // test1
@@ -19,14 +20,22 @@ console.log(solution(numbers1, k1));
 
 // test1
 const numbers2 = [1, 2, 3, 4, 5, 6];
-const k2 = 2;
+const k2 = 5;
 console.log(solution(numbers2, k2));
 
+// test3
+const numbers3 = [1, 2, 3];
+const k3 = 3;
+console.log(solution(numbers3, k3));
+
 /*
-test code
+// test code
 const test = [1, 2, 3, 4];
-console.log(test[-1]);
+console.log(test[7]);
 */
 
-// 1. 공을 던지면서 지나가는 사람 수 구하기
-// 2. 지나가는 사람 수에서 배열 길이보다 작아질때까지 감소시키기
+// PASS
+// 문제 이해 ㅜㅜ
+// 1. k - 1 만큼 2 더하기
+// 1-1. 누적합계가 배열 길이보다 길어지면 배열 길이 빼기
+// 2. k번째로 공을 던지는 사람의 번호 구하기
