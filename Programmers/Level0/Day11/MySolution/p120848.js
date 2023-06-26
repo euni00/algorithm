3e210a2ddfcb93e64694726223371760aea42734
yeeunlee <euneee2000@naver.com>
2023-06-26 23:50:10 +0900

    Studying Day11 array programming

--- Programmers/Level0/Day11/MySolution/p120848.js
+++ Programmers/Level0/Day11/MySolution/p120848.js
@@ -1,22 +1,27 @@
 // 팩토리얼
 // 스스로 해결하기
 function solution(n) {
-  let maxPac = 0;
-  let pac = Math.max();
+  // let maxPac = 0;
+  // let pac = 1;
   let pacNum = 1;
-
-  // 1-2. 팩토리얼 값이 n보다 커질때까지 반복
-  while (n < pac) {
-    // 1. 팩토리얼 구하기
-    // 1-1. 숫자를 하나씩 줄이면서 곱한 값을 누적하기
-    for (let j = pacNum; j >= 1; j--) {
-      pac *= j;
+  while (true) {
+    let pac = 1;
+    for (let i = pacNum; i >= 1; i--) {
+      pac *= i;
     }
-    pacNum++;
+
+    // pac *= pacNum;
+
+    if (pac <= n) {
+      pacNum++;
+    } else {
+      break;
+    }
   }
-  // 3. 이때 그 전 팩토리얼 i을 리턴해주기
-  maxPac = pacNum - 1;
-  return maxPac;
+
+  // console.log("pacNum : ", pacNum);
+
+  return pacNum - 1;
 }
 
 // test1
@@ -27,8 +32,8 @@ console.log(solution(n1));
 const n2 = 7;
 console.log(solution(n2));
 
-// ERROR
+// PASS
 // 1. 팩토리얼 구하기
 // 1-1. 숫자를 하나씩 줄이면서 곱한 값을 누적하기
-// 1-2. 팩토리얼 값이 n보다 커질때까지 반복
-// 3. 이때 그 전 팩토리얼 i을 리턴해주기
+// 1-2. 팩토리얼 값이 n보다 크지 않을때까지 반복하기
+// 3. 팩토리얼 숫자 리턴하기
